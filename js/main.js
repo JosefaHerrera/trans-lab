bffdf95f913131aa226e1fda5ff87948a901ccca
JosefaHerrera <josefa@linuxero.cl>
2017-07-13 23:08:31 -0400

    realizando validacion en jquery

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $('.collapse').collapse()
 /*******************/
 
 //index.html validar form.
+//Expresión para validar un correo electrónico expresiones regulares
+var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+$(document).ready(function(){
+	// al hacer click en el btn-enviar, empezará a validar el formulario
+	$("#btn-enviar").click(function(){
+	//.val() obtenemos el contenido ingresado en el input correo.
+	var correo = $("#correo").val();
+	//.val() obtenemos el contenido ingresado en el input contraseña.
+	var pwd = $("#pwd").val();
+	//al realizar variables , el codigo se ve más limpio al momento de hacer validaciones largas.
 
+	});
+});
