955055f31f93d0eb8bccf6fdfceee1ef44f37c10
JosefaHerrera <josefa@linuxero.cl>
2017-07-14 01:18:38 -0400

    buscar alternativas para hacer el menu como corresponde

--- js/main.js
+++ js/main.js
@@ -5,15 +5,7 @@
 
 
 /*******************/
-/*SIDEVAR MENU*/
-$(document).ready(function() {
-	var bodyEl = $('body'),
-    	navToggleBtn = bodyEl.find('.menu-hamburguesa');
-        navToggleBtn.on('click', function(e) {
-    	bodyEl.toggleClass('active-nav');
-        e.preventDefault();
-        });
-})();
+
 
 //index.html validar form.
 //Expresión para validar un correo electrónico expresiones regulares
