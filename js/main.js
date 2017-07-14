c31ac481af9eb473849503c690acb6225d0616ad
JosefaHerrera <josefa@linuxero.cl>
2017-07-13 22:55:04 -0400

    validando inicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+//TARIFAS
+//HORARIO ALTO  $740
+//HORARIO BAJO  $640
+//HORARIO MEDIO $680
+/*preguntas acordeon*/
 $('.collapse').collapse()
+
+/*******************/
+
+//index.html validar form.
+
